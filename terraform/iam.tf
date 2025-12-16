d82c0c699356bbdbd854069948776d8b1338c054
kishore-1906 <kishoresri75803@gmail.com>
2025-12-16 18:40:10 +0530

    vpc

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,7 @@
+########################################
+# ECS EXECUTION ROLE (EXISTING – KEEP)
+########################################
+
 resource "aws_iam_role" "ecs_execution" {
   name = "${var.project_name}-ecs-execution-role"
 
@@ -16,5 +20,24 @@ resource "aws_iam_role" "ecs_execution" {
 resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role       = aws_iam_role.ecs_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+########################################
+# ECS TASK ROLE (NEW – ADD THIS)
+########################################
+
+resource "aws_iam_role" "ecs_task" {
+  name = "${var.project_name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
