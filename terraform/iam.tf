0a8483c9dd922353ae37eb800403ae68455eb302
kishore-1906 <kishoresri75803@gmail.com>
2025-12-24 17:09:51 +0530

    IAM

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,13 +13,15 @@ resource "aws_iam_role" "ecs_task" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
@@ -31,18 +33,80 @@ resource "aws_iam_role" "codedeploy" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "codedeploy.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codedeploy" {
+########################################
+# AWS MANAGED POLICY FOR CODEDEPLOY (REQUIRED)
+########################################
+resource "aws_iam_role_policy_attachment" "codedeploy_managed" {
   role       = aws_iam_role.codedeploy.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
+
+########################################
+# EXTRA INLINE POLICY (THIS FIXES YOUR ISSUE)
+########################################
+resource "aws_iam_role_policy" "codedeploy_extra" {
+  name = "${var.project_name}-codedeploy-extra"
+  role = aws_iam_role.codedeploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+
+      ################################
+      # ECS TASK SET OPERATIONS
+      ################################
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:CreateTaskSet",
+          "ecs:UpdateServicePrimaryTaskSet",
+          "ecs:DeleteTaskSet",
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskSets",
+          "ecs:DescribeTasks"
+        ]
+        Resource = "*"
+      },
+
+      ################################
+      # ALB TARGET GROUP OPERATIONS
+      ################################
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:DeregisterTargets",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeRules"
+        ]
+        Resource = "*"
+      },
+
+      ################################
+      # PASS ECS ROLES (CRITICAL)
+      ################################
+      {
+        Effect = "Allow"
+        Action = "iam:PassRole"
+        Resource = [
+          data.aws_iam_role.ecs_execution.arn,
+          aws_iam_role.ecs_task.arn
+        ]
+      }
+    ]
+  })
 }
 
